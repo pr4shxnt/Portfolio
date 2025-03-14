d6c46dade5a3ad3416c0970e8850ba1f2e913000
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-03-14 09:24:37 +0545

    damnnnnn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Projects from "./Components/Projects";
 import Footer from "./Components/Footer";
@@ -10,16 +10,15 @@ import Hireme from "./Components/Forms/Hireme";
 import ProjectCard from "./Components/Projects/ProjectCard";
 
 const App = () => {
-
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Router >
-      <DynamicTitle />
+      <Router>
+        <DynamicTitle />
         <ScrollToTop />
         <div className="container flex flex-col justify-center items-center">
           <Navbar />
           <div className="flex-grow flex flex-col justify-center items-center">
-            <Routes >
+            <Routes>
               {/* Static Pages */}
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
@@ -51,29 +50,25 @@ const App = () => {
   );
 };
 
-
-
 const DynamicTitle = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-      if (location.pathname === "/") {
-        document.title = "Home | Prashant Adhikari";
-      } else if (location.pathname === "/projects") {
-        document.title = "Projects | Prashant Adhikari";
-      } else if (location.pathname === "/hire-me") {
-        document.title = "Hire | Prashant Adhikari";
-      } else if (/^\/projects\/[\w-]+$/.test(location.pathname)) {
-        const projectId = location.pathname.split("/").pop(); // Extract projectId from the URL
-        document.title = `${decodeURIComponent(projectId)} | Prashant Adhikari`;
-      } else {
-        document.title = "404 | Prashant Adhikari";
-      }
-    }, [location]);
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.title = "Home | Prashant Adhikari";
+    } else if (location.pathname === "/projects") {
+      document.title = "Projects | Prashant Adhikari";
+    } else if (location.pathname === "/hire-me") {
+      document.title = "Hire | Prashant Adhikari";
+    } else if (/^\/projects\/[\w-]+$/.test(location.pathname)) {
+      const projectId = location.pathname.split("/").pop(); // Extract projectId from the URL
+      document.title = `${decodeURIComponent(projectId)} | Prashant Adhikari`;
+    } else {
+      document.title = "404 | Prashant Adhikari";
+    }
+  }, [location]);
 
-    return null; // This component doesn't render anything visually.
-  };
-
-
+  return null; // This component doesn't render anything visually.
+};
 
 export default App;
